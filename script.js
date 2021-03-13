21ab2fab3ac8ba8b0e6952b67dcb0f95e70a40ce
Renata <renatamoss.web@gmail.com>
2021-03-13 18:17:00 -0300

    Zerar os botões comments/refresh/likes do último comentário Tweet inserido

--- script.js
+++ script.js
@@ -1,41 +1,75 @@
-//Exercícios aula 7 
-//No refresh trocar o número e no comments trocar o número, como no like
-//Ao clicar no follow das sugestões, aumentar numero de Following e depois de 5s dar um display none na lista
+// Exercícios aula 7 
+// No refresh trocar o número e no comments trocar o número, como no like
+// Ao clicar no follow das sugestões, aumentar nÚmero de Following e depois de 5s dar um display none na lista
+// pegar o valor do campo textarea e se estiver vazio, deixar as bordas vermelhas
+// zerar os botões comments/refresh/likes do último comentário Tweet inserido
 
-function goFollow() {
-    let contentButton = document.getElementsByTagName('button'); //vai alterar os botões para Follow/Following
-    let followers = document.getElementById('followNumber'); // vai somar +1 no total de seguidores
-    let listProfile = document.getElementById('firstProfile'); //id criado dentro da primeira tag ul para interagir com o conteúdo Who to follow
 
-    if (contentButton[1].innerText == 'Follow') { //[1] índice onde está a posição do button dentro do HTML
-        contentButton[1].innerText = 'Following';
+function goFollow() { // botão Follow localizado (navba)r e Somar +1 no botão Followers
+    let contentButton = document.getElementsByTagName('button');
+    let followers = document.getElementById('followNumber');
+
+    if (contentButton[0].innerText == 'Follow') { //[1] índice onde está a posição do button dentro do HTML
+        contentButton[0].innerText = 'Following';
 
         ++followers.innerText;
 
     } else {
-        contentButton[1].innerText = 'Follow';
+        contentButton[0].innerText = 'Follow';
 
         --followers.innerText;
     }
 
-    if (contentButton[2].innerText == 'Follow') { //ação button [2]
-        contentButton[2].innerText = 'Following';
+}
 
-        addEventListener(setTimeout(function () { //evento para display none após 5s
-            listProfile.style.display = 'none';
-        }, 5000))
+function goTweet() { // Tweet (textarea) recebe value e Insere novo Tweet posição inicial
+    let contentTweet = document.getElementById('myTweet'); // textarea: recebe value ou altera para red
+    let listItem = document.getElementsByClassName('tweetItem')[0];// cada tweet individual: li
+    let listTweets = document.getElementsByClassName('tweets')[1];// //novo tweet ul, item 1, pois é o segundo item da ul
+
+    if (contentTweet.value != "") {
+        let newTweet = listItem.cloneNode(true); // vai criar uma new variável, listItem: li clone
+        newTweet.getElementsByTagName('p')[0].innerText = contentTweet.value; //essa nova variável: newTweet será um 'p'[0], que recebe o value do contentTweet: textarea
+        newTweet.getElementsByClassName('likeNumbers')[0].innerText = '0'; //alterado o id para classe para que o novo li item[0] receba innetText '0';
+        newTweet.getElementsByClassName('contRefresh')[0].innerText = '0';
+        newTweet.getElementsByClassName('contComments')[0].innerText = '0';
+        listTweets.prepend(newTweet); //esse novo Tweet ul: listTweets assumirá a posição inicial da lista
+                        
+        contentTweet.value = ""; // depois de assumir a posição inicial da lista, o textarea fica limpo
+        contentTweet.style.borderColor = 'black';
 
     } else {
-        contentButton[2].innerText = 'Follow';
-
-        listProfile.style.display = 'block';      
+        contentTweet.style.borderColor = 'red';
     }
 }
 
-function like() {
+function goFollowSug() { //'Who to follow': ação button [2] para Following e Soma +1 Followings (navbar) 
+    let contentButton = document.getElementsByTagName('button');
+    let followings = document.getElementById('followingNumber');
+
+    if (contentButton[2].innerText == 'Follow') {
+        contentButton[2].innerText = 'Following';
+
+        ++followings.innerText;
+    } else {
+        contentButton[2].innerText = 'Follow';
+
+        --followings.innerText;
+    }
+}
+
+function cleanSug() {// 'Who to follow': evento para display (none) após 1s
+    let listProfile = document.getElementById('firstProfile');
+
+    addEventListener(setTimeout(function () {
+        listProfile.style.display = 'none';
+    }, 1000))
+}
+
+function like() { // Tweets: coração vermelho 
     let tweetLiked = document.getElementById('like'); // muda a cor da imagem heart
-    let likes = document.getElementById('likeNumbers');// aumenta +1 no total de likes ao lado da imagem heart
-    let favorites = document.getElementById('favoritesNumber');//soma +1 no favorites(nav bar) qdo clica na imagem heart
+    let likes = document.getElementsByClassName('likeNumbers')[0];// aumenta +1 no total de likes ao lado da imagem heart
+    let favorites = document.getElementById('favoritesNumber');//soma +1 no favorites(nav bar) 
 
     // Exemplo de ternário: porém nesta lógica não é possível utilizar pois temos +2 aplicaçoes
     // tweetLiked.src = tweetLiked.src.indexOf('heart-red') < 0 ? './images/heart-red.svg' : './images/heart.svg';
@@ -53,9 +87,9 @@ function like() {
     }
 }
 
-function refresh() {
+function refresh() {// Tweets: refresh verde
     let tweetRefresh = document.getElementById('refresh'); // muda a cor da imagem refresh para verde
-    let numberRefresh = document.getElementById('contRefresh'); // soma +1 qdo clica na imagem refresh
+    let numberRefresh = document.getElementsByClassName('contRefresh')[0]; // soma +1 qdo clica na imagem refresh
 
     if (tweetRefresh.src.indexOf('refresh-green') == -1) {
         tweetRefresh.src = './images/refresh-green.svg';
@@ -68,9 +102,9 @@ function refresh() {
     }
 }
 
-function comments() {
+function comments() {// Tweets: comments azul
     let tweetComments = document.getElementById('comments');// muda a cor da imagem comments para azul
-    let numberComments = document.getElementById('contComments')// somo +1 qdo clica na imagem comments
+    let numberComments = document.getElementsByClassName('contComments')[0]// somo +1 qdo clica na imagem comments
 
     if (tweetComments.src.indexOf('comments-blue') == -1) {
         tweetComments.src = './images/comments-blue.svg';
@@ -82,6 +116,16 @@ function comments() {
         --numberComments.innerText;
     }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
